9acfadecea7e31eb001f90908cbf8762dd4ad85f
Rodrigo Marnon Veloso Barros de Morais <rmarnon@yahoo.com.br>
2019-09-08 15:41:03 -0300

    Recovery data sql

---
+++ src/examples/RecuperarDadosBanco.java
@@ -0,0 +1,38 @@
+package examples;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DB;
+
+public class RecuperarDadosBanco {
+
+	public static void main(String[] args) {
+
+		Connection conn =  null;		
+		Statement st = null;		
+		ResultSet rs = null;
+		
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.createStatement();
+			
+			rs = st.executeQuery("SELECT * FROM DEPARTMENT");
+			
+			while (rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " +  rs.getString("Name"));
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeStatment(st);
+			DB.closeResultSet(rs);
+			DB.closeConnection();
+		}
+	}
+}
