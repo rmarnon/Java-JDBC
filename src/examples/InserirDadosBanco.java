a74079e5bd0940e035523279258d80a54082f4f5
Rodrigo Marnon Veloso Barros de Morais <rmarnon@yahoo.com.br>
2019-09-08 22:26:05 -0300

    Insert Data and return key

--- src/examples/InserirDadosBanco.java
+++ src/examples/InserirDadosBanco.java
@@ -2,7 +2,9 @@ package examples;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.SimpleDateFormat;
 
 import db.DB;
@@ -20,21 +22,30 @@ public class InserirDadosBanco {
 		try {
 			conn = DB.getConnection();
 			st = conn.prepareStatement(//String de insercao de dados SQL deixando dados ? para depois
-			"INSERT INTO SELLER (Name, Email, BirthDate, BaseSalary, DepartmentId) VALUES (?,?,?,?,?)");
+					"INSERT INTO SELLER "
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES (?,?,?,?,?)",
+					Statement.RETURN_GENERATED_KEYS);
+					//Final -> Segundo paraametro, e sofre sobrecarga com esse metodo tipo enumerado
 			
 			//Cada ? tem sua posicao e equivale ao tipo a ser inserido no st.set(...)
-			st.setString(1, "Rodrigo Marnon");
-			st.setString(2, "rmarnon@yahoo.com.br");
-			st.setDate(3, new java.sql.Date(sdf.parse("29/04/1982").getTime()));
+			st.setString(1, "Carla Rodrigues");
+			st.setString(2, "carla@gmail.comr");
+			st.setDate(3, new java.sql.Date(sdf.parse("27/12/1991").getTime()));
 			//Data deve ser instanciado objeto java.sql.Date, convertido "parse", e no final .getTime()
-			st.setDouble(4, 1545.00);
-			st.setInt(5, 1);
+			st.setDouble(4, 1450.00);
+			st.setInt(5, 4);
 			
 			int rowsAffected = st.executeUpdate();
 			//Retorna as linhas afetadas, no caso foi inserido uma linha inteira
 			
-			System.out.println("Done! Rows affected " + rowsAffected);
-			
+			if (rowsAffected > 0) {//Se foi add 1 ou + linhas
+				ResultSet rs = st.getGeneratedKeys();//Gera key e retorna p/ um ResultSet
+				while (rs.next()) {//Pula campos e ao chegar na ultima retorna false
+					int id = rs.getInt(1);//Tabela auxiliar com uma coluna com os Ids -> 1 no lugar "Id"
+					System.out.println("Done! Id = " + id);
+				}
+			}			
 		}
 		catch (SQLException e) {
 			e.printStackTrace();
