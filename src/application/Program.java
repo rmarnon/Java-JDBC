14d885543bf4723cfac3434ede67c1e2d1aa675f
Rodrigo Marnon Veloso Barros de Morais <rmarnon@yahoo.com.br>
2019-09-10 01:59:36 -0300

    Classe Department

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department d = new Department(1, "Rodrigo");
+		
+		System.out.println(d);
+		
+	}
+
+}
